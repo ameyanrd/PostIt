8c0075c57cbc04ab70d899f7fa7e24f3c7c3174f
Shreeraksha <shreeaithal2000@gmail.com>
2020-01-27 13:06:31 +0530

    Basic User Interface Setup

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    context = {}
+    return render(request, 'main/index.html', context)
