8cdace74f585934ecde46d4b0cd4215b5a503498
Ameya Deshpande <ameyanrd@outlook.com>
2020-01-28 20:10:01 +0530

    Tried out basic login and signup

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 def index(request):
     context = {}
-    return render(request, 'main/index.html', context)
+    if request.user.is_authenticated:
+        return render(request, 'blog/index.html', context)
+    else:
+        return render(request, 'main/index.html', context)
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('index')
+    template_name = 'signup.html'
